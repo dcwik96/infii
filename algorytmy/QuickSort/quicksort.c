eb5012082f6da8e8e24ce0ef9f6aa37b54d4946b
dcwik96 <greek96@interia.pl>
2016-11-07 11:21:51 +0100

    07.11.2016

--- algorytmy/QuickSort/quicksort.c
+++ algorytmy/QuickSort/quicksort.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include <time.h>
-
+#define MLD 100000.0
 void printarr(int arr[], int size){
   int i;
   for (i=0; i < size; i++)
@@ -19,7 +19,7 @@ void createarr(int revarr1[],int revarr2[],int ranarr1[],int ranarr2[],int size)
     revarr2[i]=j;
     j--;
   }
-  for(int i = 0; i < size; i++ )
+  for(i = 0; i < size; i++ )
   {
     ranarr1[i]=rand()%100;
     ranarr2[i]=ranarr1[i];
@@ -43,6 +43,7 @@ void bubblesort(int arr[], int n)
 
 int partition (int arr[], int low, int high)
 {
+  int tmp=0;
   int pivot = arr[high];    // pivot
   int i = (low - 1);  // Index of smaller element
   for (int j = low; j <= high- 1; j++)
@@ -50,13 +51,11 @@ int partition (int arr[], int low, int high)
     if (arr[j] <= pivot)    // If current element is smaller than or equal to pivot
     {
       i++;    // increment index of smaller element
-      int tmp;
       tmp=arr[i];
       arr[i]=arr[j];
       arr[j]=tmp;
       }
     }
-    int tmp;
     tmp=arr[i+1];
     arr[i+1]=arr[high];
     arr[high]=tmp;
@@ -65,32 +64,30 @@ int partition (int arr[], int low, int high)
 
 void quicksort(int arr[], int low, int high)
 {
+  //printarr(arr,high);
   if (low < high) //low - p  high - r
   {
     int pi = partition(arr, low, high);
     quicksort(arr, low, pi - 1);
     quicksort(arr, pi + 1, high);
   }
-  printarr(arr,high);
+  //printarr(arr,high);
 }
 
 void quicksortmodification(int arr[], int low, int high,int bubsize)
 {
-  if (low < high) //low - p  high - r
+  //printarr(arr,high);
+  if (low < high && high-low+1>=bubsize)
   {
-    if(high-low+1>=bubsize)
-    {
-      int pi = partition(arr, low, high);
-      quicksortmodification(arr, low, pi - 1,bubsize);
-      quicksortmodification(arr, pi + 1, high,bubsize);
-    }
-  /*  else{
-      printarr(arr,high);
-      printf("Sortuje teraz przez bubble\n");
-      bubblesort(arr,bubsize);
-    }*/
+    int pi = partition(arr, low, high);
+    quicksortmodification(arr, low, pi - 1,bubsize);
+    quicksortmodification(arr, pi + 1, high,bubsize);
   }
-  printarr(arr,high);
+  if (low < high && high-low+1<bubsize) {
+    printarr(arr,high);
+    printf("Sortuje teraz przez bubble\n");
+    bubblesort(arr,bubsize);
+  }
 }
 
 int main() {
@@ -114,32 +111,32 @@ int main() {
             long int tv_nsec
               This is the rest of the elapsed time (a fraction of a second), represented as the number of nanoseconds. It is always less than one billion.*/
   printf("\nQS niekorzystna\n");
-  printarr(revarr1,size);
+  //printarr(revarr1,size);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksort(revarr1,0,size);   //start sorting
+  quicksort(revarr1,0,size-1);   //start sorting
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltime1=(time1.tv_sec+time1.tv_nsec/10000.0)-(time0.tv_sec+time0.tv_nsec/10000.0);   //counting final time of sorting
+  double finaltime1=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);   //counting final time of sorting
   printarr(revarr1,size);
   printf("\nQS+BS niekorzystna\n");
-  printarr(revarr2,size);
+  //printarr(revarr2,size);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksort(revarr2,0,size);   //start sorting
+  quicksortmodification(revarr2,0,size-1,bubsize);   //start sorting
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltime2=(time1.tv_sec+time1.tv_nsec/10000.0)-(time0.tv_sec+time0.tv_nsec/10000.0);   //counting final time of sorting
+  double finaltime2=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);   //counting final time of sorting
   printarr(revarr2,size);
   printf("\nQS losowa\n");
-  printarr(ranarr1,size);
+  //printarr(ranarr1,size);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksortmodification(ranarr1,0,size,bubsize);   //start sorting
+  quicksort(ranarr1,0,size-1);   //start sorting
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltimemodification1=(time1.tv_sec+time1.tv_nsec/10000.0)-(time0.tv_sec+time0.tv_nsec/10000.0);
+  double finaltimemodification1=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);
   printarr(ranarr1,size);   //printing array by function
   printf("\nQS+BS losowa\n");
-  printarr(ranarr2,size);
+  //printarr(ranarr2,size);
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksortmodification(ranarr2,0,size,bubsize);   //start sorting
+  quicksortmodification(ranarr2,0,size-1,bubsize);   //start sorting
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltimemodification2=(time1.tv_sec+time1.tv_nsec/10000.0)-(time0.tv_sec+time0.tv_nsec/10000.0);
+  double finaltimemodification2=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);
   printarr(ranarr2,size);   //printing array by function
 
   printf("Czasy sortowań:\nQuickSort  tablica odwrotna:%f\ttablica randomowa%f\n",finaltime1,finaltimemodification1);
