8e414fa55e81e467f0df9f7a80646fbd3bfa732e
dcwik96 <greek96@interia.pl>
2016-11-08 16:23:18 +0100

    08.11.16

--- algorytmy/QuickSort/quicksort.c
+++ algorytmy/QuickSort/quicksort.c
@@ -1,7 +1,9 @@
-#include "stdio.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MLD 100000.0
+#include <string.h>
+#define MLD 1000000000.0
+
 void printarr(int arr[], int size){
   int i;
   for (i=0; i < size; i++)
@@ -9,137 +11,106 @@ void printarr(int arr[], int size){
   printf("\n");
   }
 
-void createarr(int revarr1[],int revarr2[],int ranarr1[],int ranarr2[],int size){
+void createarrrev(int revarr1[],int revarr2[],int size){
   int i=0;
-  int j=size;
-  srand( time( NULL ) );
   for(i=0;i<size;i++)
   {
-    revarr1[i]=j;
-    revarr2[i]=j;
-    j--;
+    revarr1[i]=size-i;
+    revarr2[i]=revarr1[i];
   }
+  //printf("Tablica utworzona rev w funkcji: ");
+  //printarr(revarr1,size);
+}
+
+void createarrrand(int ranarr1[],int ranarr2[],int size){
+  int i=0;
+  srand( time( NULL ) );
   for(i = 0; i < size; i++ )
   {
     ranarr1[i]=rand()%100;
     ranarr2[i]=ranarr1[i];
   }
-
+  //printf("Tablica utworzona rand w funkcji: ");
+  //printarr(ranarr1,size);
 }
 
-void bubblesort(int arr[], int n)
-{
-  int i, j;
-  for (i = 0; i < n-1; i++)
-    for (j = 0; j < n-i-1; j++)
-      if (arr[j] > arr[j+1])
-      {
-        int tmp;
-        tmp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=tmp;
-      }
+void Swap(int *a, int *b){
+  int temp=*a;
+  *a=*b;
+  *b=temp;
 }
 
-int partition (int arr[], int low, int high)
-{
-  int tmp=0;
-  int pivot = arr[high];    // pivot
-  int i = (low - 1);  // Index of smaller element
-  for (int j = low; j <= high- 1; j++)
-  {
-    if (arr[j] <= pivot)    // If current element is smaller than or equal to pivot
-    {
-      i++;    // increment index of smaller element
-      tmp=arr[i];
-      arr[i]=arr[j];
-      arr[j]=tmp;
-      }
+void bubblesort(int *A, int n){
+  int i;
+  if (n==0 || n==1) return;
+  for (i=0; i<n-1; i++){
+    if (A[i+1]<A[i]) Swap(&A[i], &A[i+1]);
+  }
+  bubblesort(A, n-1);
+}
+
+int Partition(int A[], int p, int r){
+  int x=A[r];
+  int i=p-1, j;
+  for (j=p; j<=r; j++){
+    if (A[j]<=x){
+      i++;
+      Swap(&A[i], &A[j]);
     }
-    tmp=arr[i+1];
-    arr[i+1]=arr[high];
-    arr[high]=tmp;
-    return (i + 1);
+  }
+  if (i<r) return i;
+  else return i-1;
 }
 
-void quicksort(int arr[], int low, int high)
-{
-  //printarr(arr,high);
-  if (low < high) //low - p  high - r
-  {
-    int pi = partition(arr, low, high);
-    quicksort(arr, low, pi - 1);
-    quicksort(arr, pi + 1, high);
+void quicksort(int A[], int p, int r, int c){
+  int q;
+  if (p<r && r-p+1>=c){
+    q=Partition(A, p, r);
+    quicksort(A, p, q, 0);
+    quicksort(A, q+1, r, 0);
   }
-  //printarr(arr,high);
-}
-
-void quicksortmodification(int arr[], int low, int high,int bubsize)
-{
-  //printarr(arr,high);
-  if (low < high && high-low+1>=bubsize)
-  {
-    int pi = partition(arr, low, high);
-    quicksortmodification(arr, low, pi - 1,bubsize);
-    quicksortmodification(arr, pi + 1, high,bubsize);
-  }
-  if (low < high && high-low+1<bubsize) {
-    printarr(arr,high);
-    printf("Sortuje teraz przez bubble\n");
-    bubblesort(arr,bubsize);
-  }
+  if (p<r && r-p+1<c) bubblesort(A, r-p+1);
 }
 
 int main() {
-  printf("Podaj od jakiego momentu ma tablica byc sortowana by bubblesort\n");
-  int bubsize;
+  printf("Podaj od jakiego momentu ma tablica byc sortowana bubblesort'em\n");
+  int bubsize,size;
   scanf("%i",&bubsize);
-  printf("Podaj wielkosc tablicy");
-  int size;
-  scanf("%i",&size);
-  int revarr1[size];
-  int revarr2[size];
-  int ranarr1[size];
-  int ranarr2[size];
-  createarr(revarr1,revarr2,ranarr1,ranarr2,size);
+  if(bubsize>=1){
+    printf(" KIND  |   SIZE    |   ADVERSE   |    RANDOM    \n");
+    for (size=100;size<2501;size=size+100)
+    {
+      int revarr1[size];
+      int revarr2[size];
+      int ranarr1[size];
+      int ranarr2[size];
+      createarrrev(revarr1,revarr2,size);
+      createarrrand(ranarr1,ranarr2,size);
 
-  struct timespec time0, time1;
-      /*Data Type: struct timespec
-          The struct timespec structure represents an elapsed time. It is declared in time.h and has the following members:
-            time_t tv_sec
-              This represents the number of whole seconds of elapsed time.
-            long int tv_nsec
-              This is the rest of the elapsed time (a fraction of a second), represented as the number of nanoseconds. It is always less than one billion.*/
-  printf("\nQS niekorzystna\n");
-  //printarr(revarr1,size);
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksort(revarr1,0,size-1);   //start sorting
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltime1=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);   //counting final time of sorting
-  printarr(revarr1,size);
-  printf("\nQS+BS niekorzystna\n");
-  //printarr(revarr2,size);
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksortmodification(revarr2,0,size-1,bubsize);   //start sorting
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltime2=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);   //counting final time of sorting
-  printarr(revarr2,size);
-  printf("\nQS losowa\n");
-  //printarr(ranarr1,size);
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksort(ranarr1,0,size-1);   //start sorting
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltimemodification1=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);
-  printarr(ranarr1,size);   //printing array by function
-  printf("\nQS+BS losowa\n");
-  //printarr(ranarr2,size);
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
-  quicksortmodification(ranarr2,0,size-1,bubsize);   //start sorting
-  clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
-  double finaltimemodification2=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);
-  printarr(ranarr2,size);   //printing array by function
-
-  printf("Czasy sortowań:\nQuickSort  tablica odwrotna:%f\ttablica randomowa%f\n",finaltime1,finaltimemodification1);
-  printf("QuickSort+BubbleSort  tablica odwrotna:%f\ttablica randomowa%f\n",finaltime2,finaltimemodification2);
+      struct timespec time0, time1;
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
+      quicksort(revarr1,0,size-1,0);   //start sorting
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
+      double finaltime1=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);   //counting final time of sorting
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
+      quicksort(ranarr1,0,size-1,0);   //start sorting
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
+      double finaltime2=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);   //counting final time of sorting
+      if(size>=1000) printf("  QS   |   %i    |   %f  |   %f\n",size,finaltime1,finaltime2);
+      else printf("  QS   |    %i    |   %f  |   %f\n",size,finaltime1,finaltime2);
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
+      quicksort(revarr2,0,size-1,bubsize);   //start sorting
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
+      double finaltimemodification1=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time0);   //start clock
+      quicksort(ranarr2,0,size-1,bubsize);   //start sorting
+      clock_gettime(CLOCK_PROCESS_CPUTIME_ID,&time1);   //end clock
+      double finaltimemodification2=(time1.tv_sec+time1.tv_nsec/MLD)-(time0.tv_sec+time0.tv_nsec/MLD);
+      if(size>=1000) printf(" QS+BS |   %i    |   %f  |   %f\n",size,finaltimemodification1,finaltimemodification2);
+      else printf(" QS+BS |    %i    |   %f  |   %f\n",size,finaltime1,finaltime2);
+      if(size!=2500) printf("       |           |             |              \n"); // 10|13|14
+    }
+  }
+  else return 0;
   return 0;
 }
